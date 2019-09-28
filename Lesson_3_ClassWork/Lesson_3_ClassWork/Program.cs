1114497526e532c3f264f7d106595b9be2253035
annushka34 <annsamoluk82@gmail.com>
2019-09-28 18:12:02 +0300

    thread pool

---
+++ Lesson_3_ClassWork/Lesson_3_ClassWork/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lesson_3_ClassWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main thread started");
+
+            ThreadShow();
+
+            for (int i = 0; i < 10; i++)
+            {
+             //   ThreadPool.QueueUserWorkItem(WorkinMethod, i);
+                ThreadPool.QueueUserWorkItem(new WaitCallback(WorkinMethod), i);
+
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(10);
+                Console.Write("*");
+            }
+            ThreadShow();
+
+            Console.ReadKey();
+            ThreadShow();
+        }
+
+
+        static void WorkinMethod(object args)
+        {
+            Console.WriteLine("THREAD START - Id: " + Thread.CurrentThread.ManagedThreadId);
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(10);
+                Console.Write((int)args);
+            }
+            Console.WriteLine();
+        }
+
+
+        static void ThreadShow()
+        {
+            int maxIOThreads = 0;
+            ThreadPool.GetMaxThreads(out int maxThreads, out maxIOThreads);
+            Console.WriteLine($"maxThreads   {maxThreads}, maxIOThreads   {maxIOThreads}  ");
+            ThreadPool.GetAvailableThreads(out int availableThreads, out int availableIOThreads);
+            Console.WriteLine($"availableThreads   {availableThreads}, availableIOThreads   {availableIOThreads}  ");
+        }
+    }
+}
---
+++ Lesson_3_Thread/004_ThreadPool/ThreadPoolWorker.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+namespace AsyncProgramming
+{
+    internal class ThreadPoolWorker<T>
+    {
+        private readonly Func<object, T> func;
+        private T result;
+
+        public ThreadPoolWorker(Func<object, T> func)
+        {
+            this.func = func ?? throw new ArgumentNullException(nameof(func));
+            result = default;
+        }
+
+        public bool Success { get; private set; } = false;
+        public bool Completed { get; private set; } = false;
+        public Exception Exception { get; private set; } = null;
+        public T Result
+        {
+            get
+            {
+                while (Completed == false)
+                {
+                    Thread.Sleep(150);
+                }
+
+                return Success == true && Exception == null ? result : throw Exception;
+            }
+        }
+
+        public void Start(object state)
+        {
+            ThreadPool.QueueUserWorkItem(new WaitCallback(ThreadExecution), state);
+        }
+
+        private void ThreadExecution(object state)
+        {
+            try
+            {
+                result = func.Invoke(state);
+                Success = true;
+            }
+            catch (Exception ex)
+            {
+                Exception = ex;
+                Success = false;
+            }
+            finally
+            {
+                Completed = true;
+            }
+        }
+    }
+
+}
